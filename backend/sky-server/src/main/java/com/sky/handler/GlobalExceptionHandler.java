2072335b3f5a2ffb0333dabd6a6ca162304fd7ce
renruoning <ruoningr@student.unimelb.edu.au>
2026-07-29 15:22:15 +1000

    redis初步添加

--- backend/sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ backend/sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -24,13 +24,13 @@ public class GlobalExceptionHandler {
      * @return
      */
     @ExceptionHandler
-    public Result exceptionHandler(BaseException ex) {
+    public Result<?> exceptionHandler(BaseException ex) {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
 
     @ExceptionHandler
-    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+    public Result<?> exceptionHandler(SQLIntegrityConstraintViolationException ex) {
         String message = ex.getMessage();
         if (message.contains("Duplicate entry")) {
             String[] split = message.split(" ");
